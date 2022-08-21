4002695f40ae277a7728d9c8669f94df17832532
Zeinab Nasiri <96521828+zeinab-nsr@users.noreply.github.com>
2022-08-21 19:23:41 +0430

    FEATURE: add Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Header from'./components/Header/index'
+import './App.scss';
 
 function App() {
   return (
     <div className='container'>
-      
+      <Header />
     </div>  
   );
 }
